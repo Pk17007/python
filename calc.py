9f36cfa9db2f27623cc8cc89e380f7fad694a4d1
Pk17007 <niparthkhillare@gmail.com>
2026-06-03 20:57:40 +0530

    Commit-001

---
+++ calc.py
@@ -0,0 +1,9 @@
+i = int(input("Enter a number"))
+j = int(input("Enter a number"))
+
+print("1 2 3 4 5 6 7 8")
+k = int(input())
+
+if k==1: print("Addition is",i+j)
+elif k==2: print (i-j)
+elif k==3: print (i*j) 
---
+++ main.py
@@ -0,0 +1,27 @@
+print ("hello world")
+print(2,"Heloo","   ", 234, "2","34ksjaod")
+a = 12
+b = 1.1
+c = complex(1,2)
+# data types
+# none  num  str boolean
+
+print("type of a is",type(a)) # int
+print("type of b is",type(b))  # float
+print("type of c is",type(c))   # complex
+
+# list - ordered collection of datatypes
+# tuple - immutable
+# dictionary - key value pairs
+
+
+# operators
+# floor division // 
+print(15//2)
+if a==12 :print("true")
+
+# typecasting
+# implicit and explcit
+
+
+
