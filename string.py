233f3dec8af5890bd0306ef1c57904a0d8e9e715
Pk17007 <niparthkhillare@gmail.com>
2026-06-05 22:01:53 +0530

    commit-2.1

---
+++ string.py
@@ -0,0 +1,39 @@
+str = "Fruit"
+# print(str[0])
+# print(str[0:2])
+# print(str[1:4])
+# print(str[0:-4])
+# print(len(str))
+
+
+
+
+# String Methods
+
+
+# Upper and lower
+# print(str.upper())
+# # print(str.lower())
+
+
+# rstrip
+
+# trail = "Heloo!!!oo"
+# trail2 = "Heloo!!!"
+# trail3 = "Heloo!!!ooo"
+# print(trail.rstrip("o"))
+# print(trail.rstrip("!"))
+# print(trail.rstrip("!","o")) ; TypeError: rstrip expected at most 1 argument, got 2
+
+
+# replace() and split()
+
+string="abcdefgj"
+# string2 = string.replace("j","h")
+# print(string2[0:len(string2)-1])
+
+# print(string.split(string[4]))
+# print(string.split("d"))
+
+
+
